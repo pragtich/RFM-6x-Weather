9236ac39d481337e4393c252d98903167ff90859
Joris Pragt <pragtich@github>
2016-11-09 18:43:15 +0100

    First stuff of my own added

---
+++ src/RFM-6x-Weather.h
@@ -0,0 +1,22 @@
+// Updates to RFM69 module specific to weather stations reception
+// Intended for use with Alecto WS3000 and RFM65 receiver, but should work on RFM6x family and several Fine Offset type weather stations
+//
+//
+
+
+
+#ifndef RFM6xWEATHER_h
+#define RFM6xWEATHER_h
+
+#include <RH_RF69.h>
+
+class RFM6xWeather : public RH_RF69
+{
+  // public:
+  
+}
+
+
+
+
+#endif
