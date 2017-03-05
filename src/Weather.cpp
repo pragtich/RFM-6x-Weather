393b48662f14630a0367fe67a09c0a7565a26f4b
Joris Pragt <pragtich@github>
2017-03-05 16:51:36 +0100

    Switch to ring buffers for messages and mqtt

--- src/Weather.cpp
+++ src/Weather.cpp
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiSTA.h>
 #include <PubSubClient.h>
+#include <RingBuf.h>
 
 #include <RFM-6x-Weather.h>
 
@@ -12,9 +13,15 @@
 #define LEDPIN 2
 
 const char* mqtt_server = "10.0.0.99";
-const char* weather_topic = "weather/";
-#define TOPIC_LEN 99
-char topic[TOPIC_LEN];
+const char* weather_topic = "weather";
+
+
+#define MAX_MQTT_LEN 100
+typedef struct {
+  char topic[MAX_MQTT_LEN];
+  char message[MAX_MQTT_LEN];
+} mqtt_msg;
+  
 
 #define PRINT_WITH_UNIT(a, b) {\
     Serial.print(a); \
@@ -28,12 +35,45 @@ WiFiClient espClient;
 PubSubClient mqtt(espClient);
 
 uint8_t buffer[RFM6xW_PACKET_LEN], n;
+RingBuf *observations_w = RingBuf_new(sizeof(RFM6xWeather::WeatherMessage), 10);
+RingBuf *mqtt_msgs = RingBuf_new(sizeof(mqtt_msg), 20);
 
-/*
-void myisr(void){
-  rfm.handleInterrupt();
+void process_messages() {
+  if (!observations_w->isEmpty(observations_w)){
+    RFM6xWeather::WeatherMessage obs;
+
+    if (observations_w->pull(observations_w, &obs)){
+      // do stuff with my message
+      Serial.print("Observed weather at t=");
+      Serial.println(millis());
+
+      PRINT_WITH_UNIT("ID:", obs.ID);
+
+      PRINT_WITH_UNIT(obs.temp, " ℃");
+      PRINT_WITH_UNIT(obs.RH, " %");
+      PRINT_WITH_UNIT(obs.rain, " mm");
+
+      mqtt_msg msg;
+      sprintf(msg.topic, "%s/%s/T", weather_topic, String(obs.ID, HEX).c_str());
+      strcpy(msg.message, String(obs.temp).c_str());
+      mqtt_msgs->add(mqtt_msgs, &msg);
+
+      sprintf(msg.topic, "%s/%s/RH", weather_topic, String(obs.ID, HEX).c_str());
+      strcpy(msg.message, String(obs.RH).c_str());
+      mqtt_msgs->add(mqtt_msgs, &msg);
+    }
+  }
 }
-*/
+
+void send_messages(){
+  if (!mqtt_msgs->isEmpty(mqtt_msgs)){
+    mqtt_msg msg;
+
+    if (mqtt_msgs->pull(mqtt_msgs, &msg)){
+      mqtt.publish(msg.topic, msg.message);
+    }
+  }
+}
 
 void print_date(RFM6xWeather::TimeMessage *obs){
   Serial.print(obs->hour);
@@ -48,22 +88,10 @@ void print_date(RFM6xWeather::TimeMessage *obs){
 }
 
 void observed_w(struct RFM6xWeather::WeatherMessage *obs) {
-  Serial.print("Observed weather at t=");
-  Serial.println(millis());
-
-  PRINT_WITH_UNIT("ID:", obs->ID);
-
-  PRINT_WITH_UNIT(obs->temp, " ℃");
-  PRINT_WITH_UNIT(obs->RH, " %");
-  PRINT_WITH_UNIT(obs->rain, " mm");
-
-  String topic = String("weather/") + String(obs->ID, HEX) + String("/");
-  String topic_T = topic + String("T");
-  String msg_T = String(obs->temp);
-  mqtt.publish(topic_T.c_str(), msg_T.c_str());
-  /*String topic_RH = topic + String("RH");
-  String msg_RH = String(obs->RH);
-  mqtt.publish(topic_RH.c_str(), msg_RH.c_str());*/
+  if (observations_w->add(observations_w, obs) == -1){
+    Serial.println("Weather message buffer full; message dropped");
+  }
+  
   delete obs;
 }
 
@@ -157,9 +185,9 @@ void setup()
    else
     Serial.println("RFM initialized OK");
    rfm.set_weather_handler(observed_w);
-   rfm.set_time_handler(observed_t);
+   /*rfm.set_time_handler(observed_t);
    rfm.set_unknown_handler(observed_u);
-
+   */
    mqtt.setServer(mqtt_server, 1883);
    mqtt.setCallback(mqtt_callback);
    mqtt.publish("outTopic", "hello world");
@@ -171,8 +199,10 @@ void setup()
 
 void loop(){
   rfm.run();
+  process_messages();
   if (!mqtt.connected()) {
     reconnect();
   }
   mqtt.loop();
+  send_messages();
 }
